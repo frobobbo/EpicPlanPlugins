6df1c8ecda51fd95b6d7d3175eedd67493c0a77c
Brett Johnson <johnsobd@gmail.com>
2022-04-19 16:45:50 -0400

    Initial Commit

---
+++ Vault/index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Prevent direct access
+defined('PLUGINPATH') or exit('No direct script access allowed');
+
+/*
+Plugin Name: Vault
+Plugin URL: https://www.strategyplussolutions.com
+Description: EpicPlan Password Vault for Clients
+Version: 1.0
+Requires at least: 3.0
+Supports at most: 4.0
+Author: Brett Johnson
+Author URL: https://www.strategyplussolutions.com
+*/
